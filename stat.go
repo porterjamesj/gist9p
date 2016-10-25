80305107a4717f03d2768a811ec2bb23c14f1603
James Porter <james@jamesporter.me>
2016-10-24 22:27:02 -0400

    remove incorrect comment
    turns out p9's wstat does support truncating files, per
    https://swtch.com/plan9port/man/man9/stat.html. I don't how I keep
    missing these things I swear I'm looking at old versions of the
    documentation sometimes.

--- stat.go
+++ stat.go
@@ -36,31 +36,6 @@ func (gs *GistSession) Stat(ctx context.Context, fid p9p.Fid) (p9p.Dir, error) {
 }
 
 func (gs *GistSession) WStat(ctx context.Context, fid p9p.Fid, dir p9p.Dir) error {
-	// TODO this is totally bogo, we at the very least should:
-	// 1. be willing to change the name
-	// 2. intelligently log everything else the client was trying to change
-	//
-	// There's a semantic mismatch between the semantics of the FUSE
-	// API and 9p here. In FUSE, it seems to be the case that you can
-	// Setattr a file's length to 0 as a means of truncating it
-	// (https://www.cs.columbia.edu/~du/ds/homework/2013/09/26/Homework3/#setattr-open-write-and-read),
-	// whereas 9p specifies that the length of a file cannot be set
-	// (https://swtch.com/plan9port/man/man9/stat.html).
-	//
-	// I guess the way to truncate things in plan9 is by opening with
-	// O_TRUNC? I kind of like the FUSE API better here since you can
-	// truncate more flexibly. In any case it seems like we have to
-	// implement truncation a la FUSE in order to work reasonably well
-	// with 9pfuse. This might be harder than anticipated since:
-	//
-	// 1. We need a way of detecting if the client is only trying to
-	// set one attribute, and 2. there appears to be some sort of
-	// encoding issue, as far as I can tell 9pFUSE encodse the Dir
-	// entry such that it doesn't make it through to here
-	// correctly. Sigh.
-	//
-	// I am very curious to see how the linux kernel implentation
-	// handles this situation.
 	log.Println("supposed new stat:", dir)
 	return nil
 }
